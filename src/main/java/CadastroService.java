9d203ddab6a5b5e31571ef4859c11c462a148b3a
Wadje <toddnunes0@gmail.com>
2025-09-03 00:18:10 -0300

    Inclusão do Mockito + teste com sucesso (TC 006 - cadastro)

--- src/main/java/CadastroService.java
+++ src/main/java/CadastroService.java
@@ -2,8 +2,21 @@ package main.java;
 
 public class CadastroService {
 	
-    public boolean cadastrar(Cadastro cadastro) 
-    {
+	private CadastroRepositorio cadastroRepositorio;
+
+	public CadastroService(CadastroRepositorio cadastroRepositorio) 
+	{
+	        this.cadastroRepositorio = cadastroRepositorio;
+	}
+	
+	public boolean cadastrar(Cadastro cadastro) {
+       
+		if (cadastro.getSenha() == null || cadastro.getSenha().isEmpty()) 
+		{
+            return false;
+        }
+		
+		cadastroRepositorio.salvar(cadastro);
         return true;
     }
 }
--- teste/main/teste/CadastroServiceTest.java
+++ teste/main/teste/CadastroServiceTest.java
@@ -1,9 +1,12 @@
 package main.teste;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 
 import main.java.Cadastro;
+import main.java.CadastroRepositorio;
 import main.java.CadastroService;
 
 public class CadastroServiceTest {
@@ -11,13 +14,16 @@ public class CadastroServiceTest {
     // TC006 - Cadastrar usuário com informações corretas (com falha proposital)
     @Test
     void CadastrarUsuarioComSenhaVazia() {
-        CadastroService cadastroService = new CadastroService();
+    	 
+    	CadastroRepositorio cadastroRepositorio = mock(CadastroRepositorio.class);
+    	 
+        CadastroService cadastroService = new CadastroService(cadastroRepositorio);
 
-        Cadastro cadastro = new Cadastro("teste", "12 3456 7890", "Usuário", "");
+        Cadastro cadastro = new Cadastro("teste", "12 3456 7890", "Usuário", "teste123");
 
         boolean resultado = cadastroService.cadastrar(cadastro);
 
-        
-        assertFalse(resultado, "O cadastro não deveria ser aceito com senha vazia");
+        assertTrue(resultado, "O cadastro deve ser aceito com todos os dados válidos");
+        verify(cadastroRepositorio, times(1)).salvar(cadastro);
     }
 }
